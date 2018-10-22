23833f9388b38507da24edb94909fb4db406b4e2
Harrison Palmer <harrisonpalmer@Harrisons-MacBook-Pro.local>
2018-10-22 17:15:07 -0500

    changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,11 @@
+//generates random number 19-120 that player must reach
+var randomNum = Math.floor(Math.random() * (120 - 19) + 19); 
+
 //list of variables used
-var randomNum = Math.floor(Math.random() * (120 - 19) + 19); //generates random number 19-120 that player must reach
 var wins = 0;
 var losses = 0;
 var currentScore = 0;
+
 //generates random number 1-12 for each of the crystal images
 var img1 = Math.floor(Math.random() * 12) + 1;
 var img2 = Math.floor(Math.random() * 12) + 1;
@@ -11,48 +14,47 @@ var img4 = Math.floor(Math.random() * 12) + 1;
 
 
 //function that resets random 1-12 number for crystal images
-function randomcrystalNum() {
+function randomCrystalNum() {
     img1 = Math.floor(Math.random() *(12 - 1 + 1) + 1);
     img2 = Math.floor(Math.random() *(12 - 1 + 1) + 1);
     img3 = Math.floor(Math.random() *(12 - 1 + 1) + 1);
     img4 = Math.floor(Math.random() *(12 - 1 + 1) + 1);
+    $("#img1").text(img1);
+    $("#img2").text(img2);
+    $("#img3").text(img3);
+    $("#img4").text(img4);
 } 
 
-//function used to generate new random number after win or loss
+//function used to generate new random number after win or loss and then display new number
 function randomNumGen() {
     randomNum = Math.floor(Math.random() * (120 - 19) + 19);
     $('#randomNum').text(randomNum);
 }
+
 //keeps track of player wins
 function gamePlay(){
-	if(currentScore === randomNum){
+	if (currentScore === randomNum){
 		wins++;
         $('#wins').text(wins);//displays win in html
-    
-        //$('#wins-or-losses').text('You win!');
-		//randomNum = Math.floor(Math.random()*(120 - 19 + 1) + 19);
-       // $('#randomNum').text(randomNum);
 
         //next 2 lines reset the current score and displays that in html
 		currentScore = 0;
         $('#player-currentNum').text(currentScore); 
         alert("You win, you are a regular crystal baller!");
-        randomNumGen(); //call funtion to reset rando number after win
         randomCrystalNum();  //call function to reset random number for crystal images
-    }  else if(currentScore > randomNum){   //keeping track of player losses
+        randomNumGen(); //call funtion to reset random number after win
+    }  
+    
+    else if (currentScore > randomNum){   //keeping track of player losses
 		losses++;
         $('#losses').html(losses); //displays loss in html
-        
-		//$('#wins-or-losses').text('You lose!');
-		//radnomNum = Math.floor(Math.random()*(120 - 19 + 1) + 19);
-       // $('#randomNum').text(randomNum);
 
-          //next 2 lines reset the current score and displays that in html
+        //next 2 lines reset the current score and displays that in html
 		currentScore = 0;
         $('#player-currentNum').text(currentScore);
         alert("You lose, and now you must continue onwards for humankind's fate rest in your hands...");
-        randomNumGen(); //call function to reset random number after loss
         randomCrystalNum(); // call function to reset random number for crystal images
+        randomNumGen(); //call function to reset random number after loss
 	}
 }
 
@@ -90,6 +92,10 @@ $('#img4').on('click', function(){
 });
 
 
+
+
+//code I tried before final product
+
 //var crystalNum = Math.floor(Math.random() * 12) + 1;
 
 
@@ -114,6 +120,13 @@ $("#player-currentNum").text(crystalNum);
 });
 */
 
+//reset after play win
 
+// $('#wins-or-losses').text('You win!');
+		//randomNum = Math.floor(Math.random()*(120 - 19 + 1) + 19);
+       // $('#randomNum').text(randomNum);
 
-
+       //reset after player loss
+//$('#wins-or-losses').text('You lose!');
+		//radnomNum = Math.floor(Math.random()*(120 - 19 + 1) + 19);
+        //$('#randomNum').text(randomNum);
